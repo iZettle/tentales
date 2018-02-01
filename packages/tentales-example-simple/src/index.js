982faeef83e476e9a812dae457389a3cc0e80b5b
Emil Oberg <emil@bitupnorth.com>
2018-02-01 16:19:18 +0100

    Fix simple example config

--- packages/tentales-example-simple/src/index.js
+++ packages/tentales-example-simple/src/index.js
@@ -8,16 +8,16 @@ tenTales({
   reactComponentsDirectory: path.join(__dirname, "components"),
   services: {
     renderer: {
-      host: "localhost",
+      host: "this",
       path: "/tt/renderer"
       // cache: "s3"
     },
     data: {
-      host: "localhost",
+      host: "this",
       path: "/tt/data"
     },
     editor: {
-      host: "localhost",
+      host: "this",
       path: "/tt/editor"
     }
   }
