71c75b263b584b33430a9ac893ec0ae9020bca99
Emil Oberg <emil@bitupnorth.com>
2018-03-05 11:17:53 +0100

    Add 404 mw test

--- packages/tentales/src/middleware/middlewares/four-oh-four-middleware.ts
+++ packages/tentales/src/middleware/middlewares/four-oh-four-middleware.ts
@@ -2,6 +2,7 @@ import { Middleware } from "tentales"
 
 export const fourOhFourMiddleware: Middleware = () =>
   async function actualFourOhFourMiddleware(ctx) {
+    ctx.status = 404
     ctx.body = "Nothing was found"
   }
 
